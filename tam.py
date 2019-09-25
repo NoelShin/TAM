4c1f77cc18da61f048189bb0fb7b265512a7f220
Noel <36836739+NoelShin@users.noreply.github.com>
2019-09-25 22:24:21 +0900

    Update tam.py

--- tam.py
+++ tam.py
@@ -4,7 +4,7 @@ from math import log
 
 
 class TAM(nn.Module):
-    def __init__(self, n_ch, group_size, bypass=False):
+    def __init__(self, n_ch, group_size):
         super(TAM, self).__init__()
         tam = []
         for i in range(int(log(n_ch) / log(group_size))):
@@ -17,10 +17,6 @@ class TAM(nn.Module):
         tam += [nn.Conv2d(n_ch, 1, 1)] if n_ch != 1 else []
         tam += [nn.Sigmoid()]
         self.tam = nn.Sequential(*tam)
-        self.bypass = bypass
 
     def forward(self, x):
-        if self.bypass:
-            return x
-        else:
-            return x * self.tam(x)
+        return x * self.tam(x)
